26fc2c1ab6dad58d0b7e1bc6b33a8acc0ea45a66
redko4u <kondavenkat537@gmail.com>
2024-05-27 20:24:47 +0530

    conditd

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,14 +1,11 @@
-#! bin/bash
+#!/bin/bash
 
+# check if the number is greater than 10 or not
 NUMBER=$1
 
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then
-    echo "$NUMBER Is Greater Then 10"
-
+    echo "$NUMBER is greater than 10"
 else
-
-    echo "$NUMBER Is Lesser Than 10"
-
+    echo "$NUMBER is not greater than 10"
 fi
-
