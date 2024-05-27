5a44640f9123b720b08191eac125e32784337a51
redko4u <kondavenkat537@gmail.com>
2024-05-27 18:07:54 +0530

    01 file

--- 01-variables.sh
+++ 01-variables.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-PERSON1=SURESH
-PERSON2=RAMESH
+echo "Hello World, I am learning DevOps with AWS"
+PERSON1=Sachin
+PERSON2=Rohit
 
-echo "$PERSON1 : HI $PERSON2 Very Good Day"
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
