c80bae1c375af42cf71123f4f5960035a3b00ede
redko4u <kondavenkat537@gmail.com>
2024-05-28 16:42:08 +0530

    05-vardes

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,13 +2,13 @@
 
 echo "Please enter your username"
 
-read -s USERNAME # this is the variable where the value of the one user entered
+read USERNAME # this is the variable where the value of the one user entered
 
 #this is only for validation, we should not print in the terminal
 echo "Username entered is: $USERNAME"
 
 echo "Please enter your password"
 
-read -s PASSWORD
+read PASSWORD
 
 echo "Password entered is: $PASSWORD"
