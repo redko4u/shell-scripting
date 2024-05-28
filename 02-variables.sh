73fb7483c501f5527b8853363067d398bf7e0c38
redko4u <kondavenkat537@gmail.com>
2024-05-28 15:07:09 +0530

    variables2

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
-# check if the number is greater than 10 or not
-NUMBER=$1
-
-if [ $NUMBER -gt 10 ]
-then
-    echo "$NUMBER is greater than 10"
-else
-    echo "$NUMBER is not greater than 10"
-fi
+Date = $(date)
+echo -e "\e[32m Today Date Is $Date"
