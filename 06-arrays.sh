b136bc7adb4e4eb373a0551b9e9966ac8c115874
redko4u <kondavenkat537@gmail.com>
2024-05-28 20:34:45 +0530

    06-arrays-final-outputs

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -2,5 +2,5 @@
 FRUITS=("BANANA" "MANGO" "APPLE")
 echo "FIRST FRUIT: ${FRUITS[0]}"
 echo "ALL FRUITS: ${FRUITS[@]}"
-echo "NUMBER OF FRUITS : $# "
+echo "NUMBER OF FRUITS : ${#FRUITS[@]}"
 echo "The Sscript Name Is : $0 "
