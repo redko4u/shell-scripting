0a71a54d585a712a54cce3dfec0b9354bdca8a2d
redko4u <kondavenkat537@gmail.com>
2024-05-28 16:15:00 +0530

    03-variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PESRON1=$1
 PERSON2=$2
-echo "$PERSON1 hi $PERSON2 How Are You"
-echo "$PERSON2 i am fine how about you"
-echo "$PERSON1 i am good where are you going $PERSON2"
-echo "$PERSON2 I am going to us $PERSON1"
+echo "$PERSON1: hi $PERSON2 How Are You"
+echo "$PERSON2: i am fine how about you"
+echo "$PERSON1: i am good where are you going $PERSON2"
+echo "$PERSON2: I am going to us $PERSON1"
