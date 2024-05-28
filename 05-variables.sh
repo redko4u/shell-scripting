4199e5a1bb37ad1754ad49952c434ae054c02ec7
redko4u <kondavenkat537@gmail.com>
2024-05-28 16:45:06 +0530

    05-vards

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-echo "Please enter your username"
+LOGINID=$1
+PASS=$2
+echo "Please enter your username$LOGINID"
 
-read USERNAME # this is the variable where the value of the one user entered
+read $LOGINID # this is the variable where the value of the one user entered
 
 #this is only for validation, we should not print in the terminal
-echo "Username entered is: $USERNAME"
+echo "LoginID entered is: $LOGINID"
 
 echo "Please enter your password"
 
-read PASSWORD
+read $PASS
 
-echo "Password entered is: $PASSWORD"
+echo "Password entered is: $PASS"
