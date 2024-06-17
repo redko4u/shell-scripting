1249a3dec025c002803ff94d16abb6dace5cf1e0
redko4u <kondavenkat537@gmail.com>
2024-06-17 07:54:21 -0700

    install of sql

---
+++ 08-conditions.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id-u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "ERROR: Please RUN With ROOT ACCESS"
+    exit 1
+fi
+
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "Installation Of MySQL Is Error"
+    exit 1
+else 
+    echo "Instalallation Of SQL SUCCESS"
+fi
